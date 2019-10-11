1ab229cfed6ce7ddc1a7c0fc135d544615bd9b5b
MASATO-YAKUSHIGAWA <m.yks.022@gmail.com>
2019-10-11 20:13:21 +0900

    create toppage

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,15 @@
 class PortfoliosController < ApplicationController
 
   def index
+  end
 
+  def no_1
+  end
+
+  def no_2
+  end
+  
+  def no_3
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root "portfolios#index"
 
+  get '/1',   to: 'portfolios#no_1'
+  get '/2',   to: 'portfolios#no_2'
+  get '/3',   to: 'portfolios#no_3'
 end
